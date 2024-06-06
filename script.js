d0dafad9687e469d4398c8fad92d91fa2fb01ac8
Mpilenhle Mtaka <inquire.enhle@gmail.com>
2024-06-06 20:20:58 +0200

    working register-link

--- script.js
+++
@@ -1,9 +0,0 @@
-function showRegisterForm() {
-    document.getElementById('login-form').style.display = 'none';
-    document.getElementById('register-form').style.display = 'block';
-}
-
-function showLoginForm() {
-    document.getElementById('register-form').style.display = 'none';
-    document.getElementById('login-form').style.display = 'block';
-}
