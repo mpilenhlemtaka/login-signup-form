4b07698badc4de8d4068a74a8d92cb81f4f7ea2f
Mpilenhle Mtaka <inquire.enhle@gmail.com>
2024-06-08 14:21:43 +0200

    updated js

---
+++ firebaseauth.js
@@ -0,0 +1,86 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyALBrZ-0PtXml405BcuM7c6raFYtFaIAA4",
+    authDomain: "loginregisterform-mpilo.firebaseapp.com",
+    databaseURL: "https://loginregisterform-mpilo-default-rtdb.firebaseio.com",
+    projectId: "loginregisterform-mpilo",
+    storageBucket: "loginregisterform-mpilo.appspot.com",
+    messagingSenderId: "373146559570",
+    appId: "1:373146559570:web:11be7fe568117a238a2b8b",
+    measurementId: "G-E396MR5NYN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+function showMessage(message, divId) {
+    const messageDiv = document.getElementById(divId);
+    messageDiv.style.display = "block";
+    messageDiv.innerHTML = message;
+    messageDiv.style.opacity = 1;
+    setTimeout(function () {
+        messageDiv.style.opacity = 0;
+    }, 5000);
+}
+
+document.getElementById('signUp').addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = document.getElementById('rEmail').value;
+    const password = document.getElementById('rPassword').value;
+    const fullName = document.getElementById('fName').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            const userData = {
+                email: email,
+                fullName: fullName,
+            };
+            showMessage('Account Created Successfully', 'signUpMessage');
+            const docRef = doc(db, "users", user.uid);
+            setDoc(docRef, userData)
+                .then(() => {
+                    window.location.href = 'index.html';
+                })
+                .catch((error) => {
+                    console.error("error writing document", error);
+                });
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            if (errorCode === 'auth/email-already-in-use') {
+                showMessage('Email Address Already Exists !!!', 'signUpMessage');
+            } else {
+                showMessage('Unable to create User', 'signUpMessage');
+            }
+        });
+});
+
+document.getElementById('submitSignIn').addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            showMessage('Login is successful', 'signInMessage');
+            const user = userCredential.user;
+            localStorage.setItem('loggedInUserId', user.uid);
+            window.location.href = 'homepage.html';
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            if (errorCode === 'auth/invalid-credential') {
+                showMessage('Incorrect Email or Password', 'signInMessage');
+            } else {
+                showMessage('Account does not Exist', 'signInMessage');
+            }
+        });
+});
+
---
+++ script.js
@@ -0,0 +1,9 @@
+function showRegisterForm() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'block';
+}
+
+function showLoginForm() {
+    document.getElementById('register-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+}
